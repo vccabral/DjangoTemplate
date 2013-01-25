1617ed05098ce1c8b6bed3f1c0a881de303177ab
Victor Cabral <vccabral@gmail.com>
2013-01-25 18:18:39 -0500

    missing redirect to template engine

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,8 +1,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-
-from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import direct_to_template, redirect_to
 
 from django.contrib import admin
 admin.autodiscover()
